301ec1c9e419577656eed9733ab5fa69d7eb7958
Mmulobi <mosesmutush7@gmail.com>
2022-12-08 01:16:30 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/*
+ * print a C progrm using puts 
+ * printf function should not be used
+ *return:0
+ *
+ */
+
+int main(void) 
+{
+	puts("programming is like building a multilingual puzzle\n");
+	return (0);
+} 
