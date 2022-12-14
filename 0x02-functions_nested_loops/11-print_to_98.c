ead680393661e22127ef91b0b6b6e92e302350a5
Mmulobi <mosesmutush7@gmail.com>
2022-12-14 04:42:25 -0800

    C functions, nested loops

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_to_98 - prints all natural numbers upto 98
+ * @n: The number to begin counting from
+ *
+*/
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);
+	}
+	else
+	{
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,6 +11,7 @@ int print_last_digit(int n);
 void jack_bauer(void);
 void times_table(void);
 int add(int num1, int num2);
+void print_to_98(int n);
 
 
 #endif
