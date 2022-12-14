ad2d8c5ff224f7d1924f8e4b5fe2fdcb2b9fc515
Mmulobi <mosesmutush7@gmail.com>
2022-12-14 04:56:02 -0800

    C functions, nested loops

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Lists all natural numbers below 1024
+ *
+ * Return: Always 0
+*/
+int main(void)
+{
+	int i, sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum += i;
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
