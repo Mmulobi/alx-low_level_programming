a3ca5eeeaa6d219bab7ee49ce595249844fceb86
Mmulobi <mosesmutush7@gmail.com>
2022-12-15 08:52:14 -0800

    program that prints triangle squares

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * print_triangle - prints a triangle of squares
+ * @size: The size of square triangle
+ *
+ * Return: empty
+ */
+void print_triangle(int size)
+{
+	int a, b, c;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (a = 0; a < size; a++)
+		{
+			for (b = size - a; b > 1; b--)
+			{
+				_putchar(32);
+			}
+
+			for (c = 0; c <= a; c++)
+			{
+				_putchar(35);
+			}
+
+			_putchar('\n');
+		}
+	}
+}
