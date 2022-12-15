6aa934ccfc36570ef5f6cfde77119c3acc7f634f
Mmulobi <mosesmutush7@gmail.com>
2022-12-15 04:05:54 -0800

    program prints squares

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_square - prints squares
+ * @size: The number of squares
+ *
+ * Return: empty
+ */
+void print_square(int size)
+{
+	int a, b;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (a = 0; a < size; a++)
+		{
+			for (b = 0; b < size; b++)
+			{
+				_putchar(35);
+			}
+
+			_putchar('\n');
+		}
+	}
+}
