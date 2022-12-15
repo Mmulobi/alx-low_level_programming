47d08344b9a5b19c9187a0daf87cb755ef7a4c27
Mmulobi <mosesmutush7@gmail.com>
2022-12-15 03:00:17 -0800

    program prints mutiplication of two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - Mutiplies twi integer numbers
+ * @t: The first number to mutiply
+ * @v: The second number to mutiply with first number
+ *
+ * Return: The result of mutiplication between t and v
+ */
+int mul(int t, int v)
+{
+	return (t * v);
+}
