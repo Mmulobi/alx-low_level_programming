33b9a6f91bac687f6bf61bd2bd4a3ac24f3723b4
Mmulobi <mosesmutush7@gmail.com>
2022-12-15 04:09:12 -0800

    program that prints squares

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -10,7 +10,10 @@ void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
-void print_suare(int size);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+
 
 
 #endif
