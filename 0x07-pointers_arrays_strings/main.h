9ba3bda55e240597c67d896f7389545c5fcc7f64
Mmulobi <mosesmutush7@gmail.com>
2023-01-03 04:02:13 -0800

    function that fills a memory with a constant byte

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+char *memset(char *s, char b, unsigned int n);
+char *memcpy(char *dest, char src, unsigned int n);
+char *strch(char *s, char *s);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+
+
+
+
+
+#endif
