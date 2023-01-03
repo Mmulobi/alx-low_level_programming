72bde47b3332189e5dfb53d912cd1b7684efebe3
Mmulobi <mosesmutush7@gmail.com>
2023-01-03 04:21:12 -0800

    task0

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -9,9 +9,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
-
-
-
-
-
 #endif
