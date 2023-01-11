d527bd62c1798722e87e361019d80e324a65974a
Mmulobi <mosesmutush7@gmail.com>
2023-01-11 17:13:14 +0300

    creates grid

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * free_grid - free a two dimentional grid
+ * @grid: the given grid
+ * @height: the given height
+ *
+ * Return: void
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	i = 0;
+
+	while (i < height)
+	{
+		free(grid[i]);
+		i++;
+	}
+
+	free(grid);
+}
