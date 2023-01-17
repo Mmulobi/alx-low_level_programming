e290cd72f508c2fd6998ce2bff20c61717d52216
Mmulobi <mosesmutush7@gmail.com>
2023-01-16 23:33:29 -0800

    file name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints the name of the file it was compliled from
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("%s\n", _FILE_);
+	return (0);
+}
