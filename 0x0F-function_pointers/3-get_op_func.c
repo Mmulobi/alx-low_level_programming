63f6803ae48d343934e269cd85df1fd6ef1aa0d9
Mmulobi <mosesmutush7@gmail.com>
2023-01-19 05:46:30 -0800

    task 3

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,29 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * get_op_func - functions that return a basic operation
+ * @s: char pointer
+ * Return: the function selected
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (i < 5)
+	{
+		if (ops[i].op[0] == s[0])
+			return (ops[i].f);
+
+		i++;
+	}
+	return (NULL);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,53 @@
+#include "3-calc.h"
+#include <stdio.h>
+/**
+ * op_add - function that prints a name
+ * @a: pointer to a char
+ * @b: pointer to a function
+ * Return: a+b
+ */
+int op_add(int a, int b)
+{
+		return (a + b);
+}
+/**
+ * op_sub - function that prints a name
+ * @a: pointer to a char
+ * @b: pointer to a function
+ * Return: a-b
+ */
+int op_sub(int a, int b)
+{
+		return (a - b);
+}
+/**
+ * op_mul - function that prints a name
+ * @a: pointer to a char
+ * @b: pointer to a function
+ * Return: a*b
+ */
+int op_mul(int a, int b)
+{
+		return (a * b);
+}
+/**
+ * op_div - function that prints a name
+ * @a: pointer to a char
+ * @b: pointer to a function
+ * Return: a/b
+ */
+int op_div(int a, int b)
+{
+		return (a / b);
+}
+/**
+ * op_mod - function that prints a name
+ *
+ * @a: pointer to a char
+ * @b: pointer to a function
+ * Return: a%b
+ */
+int op_mod(int a, int b)
+{
+		return (a % b);
+}
