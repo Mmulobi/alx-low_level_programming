8dd7d02529d3508daf181dc1ec770c8ad5dc4758
Mmulobi <mosesmutush7@gmail.com>
2023-01-19 17:26:10 +0300

    task 3

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,76 +1,52 @@
 #include "3-calc.h"
-#include <stdlib.h>
 #include <stdio.h>
-
 /**
-  * op_add - ...
-  * @a: ...
-  * @b: ...
-  *
-  * Return: ...
-  */
+ * op_add - function that prints a name
+ * @a: pointer to a char
+ * @b: pointer to a function
+ * Return: a+b
+ */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
-
 /**
-  * op_sub - ...
-  * @a: ...
-  * @b: ...
-  *
-  * Return: ...
-  */
+ * op_sub - function that prints a name
+ * @a: pointer to a char
+ * @b: pointer to a function
+ * Return: a-b
+ */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
-
 /**
-  * op_mul - ...
-  * @a: ...
-  * @b: ...
-  *
-  * Return: ...
-  */
+ * op_mul - function that prints a name
+ * @a: pointer to a char
+ * @b: pointer to a function
+ * Return: a*b
+ */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
-
 /**
-  * op_div - ...
-  * @a: ...
-  * @b: ...
-  *
-  * Return: ...
-  */
+ * op_div - function that prints a name
+ * @a: pointer to a char
+ * @b: pointer to a function
+ * Return: a/b
+ */
 int op_div(int a, int b)
 {
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-
 	return (a / b);
 }
-
 /**
-  * op_mod - ...
-  * @a: ...
-  * @b: ...
-  *
-  * Return: ...
-  */
+ * op_mod - function that prints a name
+ * @a: pointer to a char
+ * @b: pointer to a function
+ * Return: a%b
+ */
 int op_mod(int a, int b)
 {
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-
 	return (a % b);
 }
-
