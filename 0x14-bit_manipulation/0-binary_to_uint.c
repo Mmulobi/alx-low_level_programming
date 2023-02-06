dee932832fc4ebfb235ea828a12e3889bad339cb
Mmulobi <mosesmutush7@gmail.com>
2023-02-06 02:26:52 -0800

    binary

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -23,3 +23,17 @@ unsigned int binary_to_uint(const char *b)
 	}
 	return (num);
 }
+/**
+ * _pow_recursion - returns the value of x raised to the power of y
+ * @x: base number
+ * @y: exponent number
+ * Return: the power of x
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}
