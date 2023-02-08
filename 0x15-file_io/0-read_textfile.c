1a2ca95d233194b2613a7255528d41b07fb31343
Mmulobi <mosesmutush7@gmail.com>
2023-02-08 03:10:26 -0800

    fix task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -19,7 +19,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (filename == NULL && *filename == '\0')
 		return (0);
-	fd = open(filename, 0_RDONLY);
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 	buffer = malloc(letters * sizeof(char));
