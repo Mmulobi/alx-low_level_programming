3ea8f075ecc8079a443bea7a0d6ca61dbe52a669
Mmulobi <mosesmutush7@gmail.com>
2023-02-08 04:22:53 -0800

    fix append

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -23,7 +23,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 	{
 		close(fd);
-		return (-1);
+		return (1);
 	}
 	len = _strlen(text_content);
 	aux = write(fd, text_content, len);
